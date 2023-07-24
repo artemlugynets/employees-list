0b6afeb00df1c4de27f0f2029a88002569aaa4be
Artem Lugynets <artemlugynets@gmail.com>
2023-07-24 18:51:39 +0300

    Initial Commit

---
+++ employees-list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  employees-list
+//
+//  Created by Artem Lugynets on 24.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
